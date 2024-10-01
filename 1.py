e33ac911cb845269bdd3f009088c47b46c592b4f
_akash.jana__ <102944323+akashjana2123@users.noreply.github.com>
2024-10-01 11:10:12 +0530

    Ticket show work 1

--- 1.py
+++ 1.py
@@ -1,1 +1,14 @@
-print("Hello World!")
+import json
+
+def main():
+    with open("storafe_(file.json","r")as file:
+        data=json.load(file)
+    query=input("Enter what you want to view:\n")
+    if query in data:
+        print(f"your responce is {data[qery]}")
+    else:
+        print("The requesed responce is not present\n")
+
+
+if __name__=="__main__":
+    main()
